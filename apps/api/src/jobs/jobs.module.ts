06e4a75a95c049f1f4b9a8079506a790110899a7
floodgate46 <187267979+Floodgate46@users.noreply.github.com>
2026-08-19 14:52:04 +0100

    fix: add EmailModule import to JobsModule for WeeklyKpiSummaryJob DI

--- apps/api/src/jobs/jobs.module.ts
+++ apps/api/src/jobs/jobs.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
+import { EmailModule } from '../email/email.module';
 import { RevenueSnapshotService } from './revenue-snapshot.service';
 import { RevenueSnapshotJob } from './revenue-snapshot.job';
 import { WeeklyKpiService } from './weekly-kpi.service';
@@ -10,7 +11,7 @@ import { AutoCancelRidesService } from './auto-cancel-rides.service';
 import { AutoCancelRidesJob } from './auto-cancel-rides.job';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), PrismaModule, RedisModule],
+  imports: [ScheduleModule.forRoot(), PrismaModule, RedisModule, EmailModule],
   providers: [
     WeeklyKpiService,
     RevenueSnapshotService,
